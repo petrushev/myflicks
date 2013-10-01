b7251119a76422de365d48e23627b29b246b828a
Baze Petrushev <b.petrushev@gmail.com>
2013-10-01 22:20:07 +0200

    Added requirements: requests, lxml

--- setup.py
+++ setup.py
@@ -6,5 +6,6 @@ setup(name='myflick',
       author='Baze Petrushev',
       author_email='b.petrushev@gmail.com',
       url='http://www.python.org/sigs/distutils-sig/',
-      install_requires=['psycopg2', 'Werkzeug', 'Jinja2', 'SQLAlchemy', 'oauth2']
+      install_requires=['psycopg2', 'Werkzeug', 'Jinja2', 'SQLAlchemy',
+                        'oauth2', 'requests', 'lxml']
 )
